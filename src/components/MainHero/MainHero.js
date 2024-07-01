695cbef88a36bf3481576c0672ec25e5b6630608
Stanislav Kruvianchuk <kruvianchuk@gmail.com>
2024-07-01 13:38:09 +0300

    add planet

--- src/components/MainHero/MainHero.js
+++ src/components/MainHero/MainHero.js
@@ -6,9 +6,10 @@ const MainHero = () => {
     return (
         <div className='hero'>
             <div className='hero__subtitle'>Hero Subtitle</div>
-            <h1 className='hero__title'>Title</h1>
-            <img className='hero__icon' src={heroIcon} alt='hero-icon' /> 
-            <div className='hero__icon-description'>image desr</div>
+            <h1 className='hero__title'>Simple title</h1>
+            <img className='hero__icon' src={heroIcon} alt='hero-icon' />
+            <div className='hero__icon-description'>Image desr</div>
+            <div className='hero__planet'></div>
         </div>
     )
 }
