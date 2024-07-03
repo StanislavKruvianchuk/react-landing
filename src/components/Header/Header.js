f7fd024d4068e4d0bdf6defc13854cce14fd86e7
Stanislav Kruvianchuk <kruvianchuk@gmail.com>
2024-07-03 14:27:06 +0300

    add icons to menu

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,26 +1,37 @@
 import { FaReact } from "react-icons/fa";
 import { IoPersonSharp } from "react-icons/io5";
-import { AiFillCalendar } from "react-icons/ai";
+import { AiFillCalendar, AiFillSetting, AiFillSun } from "react-icons/ai";
 import './header.scss'
 
 const Header = () => {
+    const showMenu = (e) => {
+        const iconsBox = document.getElementById('icons-block');
+        iconsBox.classList.toggle('header__icons--active');
+    }   
+
+
     return (
         <header className="header">
             <div className="header__wrapper">
                 <a className="header__logo" href="/" aria-label="Logo">
                     <FaReact className="header__logo-img" />
                 </a>
-                <div className="header__burger">
-                    <div className="header__icons"> 
-                        <IoPersonSharp />
-                        <AiFillCalendar />
+                <div className="header__burger" onClick={showMenu}>
+                    <div className="header__icons" id="icons-block"> 
+                        <IoPersonSharp className="header__icons-item" />
+                        <AiFillCalendar className="header__icons-item" />
+                        <AiFillSun className="header__icons-item"/>
+                        <AiFillSetting className="header__icons-item" />
                     </div>
 
-                    <input className="header__burget-input" type="checkbox" />
+                    <div>
+                        <input className="header__burget-input" type="checkbox" />
 
-                    <span className="header__burger-stick"></span>
-                    <span className="header__burger-stick"></span>
-                    <span className="header__burger-stick"></span>
+                        <span className="header__burger-stick"></span>
+                        <span className="header__burger-stick"></span>
+                        <span className="header__burger-stick"></span>
+                    </div>
+                    
                 </div>
             </div>
         </header>
